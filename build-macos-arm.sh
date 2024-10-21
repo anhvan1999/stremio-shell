92771201bf769608c4b4780263345699909cf4cc
Van Dang <vadang@axon.com>
2024-10-21 22:05:56 +0700

    update

---
+++ build-macos-arm.sh
@@ -0,0 +1,23 @@
+brew install mpv ffmpeg node cmake qt@5
+
+brew install --cask qt-creator
+
+npm install -g appdmg
+
+git submodule update --init --recursive
+
+brew install openssl
+
+brew link openssl --force
+
+brew link qt5 --force
+
+qmake CONFIG+=sdk_no_version_check
+
+cmake -DCMAKE_PREFIX_PATH=/opt/homebrew/opt/qt@5 .
+
+cmake --build .
+
+sh ./mac/finalize.sh
+
+sh ./mac/pack.sh
